5821e8214c2fbd7d3e0abad8815c988b4fac4cc3
Mateusz Trzuskowski <mateusz.trzuskowski.91@gmail.com>
2021-04-17 19:56:34 +0200

    Add hello name endpoint & test

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ app = FastAPI()
 
 
 @app.get('/')
-def root():
+def root_view():
     return {'message': "Hello"}
+
+
+@app.get("/hello/{name}")
+def hello_name_view(name: str):
+    return f"Hello {name}"
 
 # to run type:
 # uvicorn main:app
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,25 @@
+import pytest
 from fastapi.testclient import TestClient
 
 from main import app
 
 client = TestClient(app)
 
-
-def test_root():
-    response = client.get('/')
-    assert response.status_code == 200
-    print(response)
-    print(response.text)
-    print(response.raw)
-    print(response.reason)
-    print(response.content)
-
 # pytest -v -W ignore::DeprecationWarning
 # -v wyświetli szczegółowe inf o testach
 # -W pozwoli ukryć warningi wymienionego typu
+
+
+def test_hello():
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Hello'}
+
+
+@pytest.mark.parametrize('name', ['Zenek', 'Janek', 'Matt', '12312@! 1'])
+def test_hello_name(name):
+    # name = "Matt"
+    response = client.get(f"/hello/{name}")
+    print('jajajajaja')
+    assert response.status_code == 200
+    assert response.text == f'"Hello {name}"'
