1500c62fbf04c32e540358513eec1af9957664c1
Mateusz Trzuskowski <mateusz.trzuskowski.91@gmail.com>
2021-05-04 20:07:28 +0200

    Ex 2 #4nd correct after CR tests

--- views/homework3.py
+++ views/homework3.py
@@ -30,14 +30,13 @@ def get_hello_html(request: Request):
 
 @homework3.post("/login_session", status_code=201)
 def post_login_session(response: Response, credentials: HTTPBasicCredentials = Depends(basic)):
-    print(credentials)
-    if credentials.username != login or credentials.password != password:
-        response.status_code = 401
-        raise HTTPException(status_code=401)
-    else:
+    if credentials.username == login or credentials.password == password:
         token_hex = get_token_hex(credentials.username, credentials.password, homework3.secret_key)
         response.set_cookie(key='session_token', value=token_hex)
         homework3.saved_session = token_hex
+    else:
+        response.status_code = 401
+        raise HTTPException(status_code=401)
     return response
 
 # @homework3.post("/login_session")
@@ -49,7 +48,6 @@ def post_login_session(response: Response, credentials: HTTPBasicCredentials = D
 
 @homework3.post("/login_token", status_code=201)
 def post_login_token(response: Response, credentials: HTTPBasicCredentials = Depends(basic)):
-    print(credentials)
     if credentials.username == login and credentials.password == password:
         token_hex = get_token_hex(credentials.username, credentials.password, homework3.secret_key)
         homework3.saved_token = token_hex
