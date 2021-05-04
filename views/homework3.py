cc1c1d70d754b348f92cec6ef1e9afc81b47b8d0
Mateusz Trzuskowski <mateusz.trzuskowski.91@gmail.com>
2021-05-04 23:29:34 +0200

    HW3 Ex5

--- views/homework3.py
+++ views/homework3.py
@@ -15,8 +15,8 @@ basic = HTTPBasic()
 
 homework3.secret_key = "secret should be long because it is more secjurne"
 login, password = '4dm1n', 'NotSoSecurePa$$'
-homework3.saved_token = None
-homework3.saved_session = None
+homework3.saved_tokens = list()
+homework3.saved_sessions = list()
 
 
 # http://127.0.0.1:8000/hello
@@ -33,7 +33,9 @@ def post_login_session(response: Response, credentials: HTTPBasicCredentials = D
     if credentials.username == login or credentials.password == password:
         token_hex = get_token_hex(credentials.username, credentials.password, homework3.secret_key)
         response.set_cookie(key='session_token', value=token_hex)
-        homework3.saved_session = token_hex
+        if len(homework3.saved_sessions) > 3:
+            del homework3.saved_sessions[0]
+            homework3.saved_sessions.append(token_hex)
     else:
         response.status_code = 401
         raise HTTPException(status_code=401)
@@ -43,7 +45,9 @@ def post_login_session(response: Response, credentials: HTTPBasicCredentials = D
 def post_login_token(response: Response, credentials: HTTPBasicCredentials = Depends(basic)):
     if credentials.username == login and credentials.password == password:
         token_hex = get_token_hex(credentials.username, credentials.password, homework3.secret_key)
-        homework3.saved_token = token_hex
+        if len(homework3.saved_tokens) > 3:
+            del homework3.saved_tokens[0]
+            homework3.saved_tokens.append(token_hex)
     else:
         response.status_code = 401
         raise HTTPException(status_code=401)
@@ -53,32 +57,32 @@ def post_login_token(response: Response, credentials: HTTPBasicCredentials = Dep
 @homework3.get('/welcome_session')
 def get_welcome_session(response: Response, session_token: str = Cookie(None), format: str = ""):
     # print(session_token)
-    # print(homework3.saved_session)
-    if session_token != homework3.saved_session:
+    # print(homework3.saved_sessions)
+    if session_token not in homework3.saved_sessions:
         raise HTTPException(status_code=401)
     return get_response_by_format(format)
 
 
 @homework3.get('/welcome_token')
 def get_welcome_session(response: Response, token: str, format: str = ""):
-    if token != homework3.saved_token:
+    if token not in homework3.saved_tokens:
         raise HTTPException(status_code=401)
     return get_response_by_format(format)
 
 
 @homework3.delete("/logout_session")
 def logout_session(session_token: str = Cookie(None), format: str = ""):
-    if session_token != homework3.saved_session:
+    if session_token not in homework3.saved_sessions:
         raise HTTPException(status_code=401)
-    homework3.saved_session = ''
+    homework3.saved_sessions.remove(session_token)
     return RedirectResponse(url=f"/logged_out?format={format}", status_code=302)
 
 
 @homework3.delete("/logout_token")
 def logout_token(session_token: str, format: str = ""):
-    if session_token != homework3.saved_token:
+    if session_token not in homework3.saved_tokens:
         raise HTTPException(status_code=401)
-    homework3.saved_token = ''
+    homework3.saved_tokens.remove(session_token)
     return RedirectResponse(url=f"/logged_out?format={format}", status_code=302)
 
 
