a3974ddd838e3fa725d799daadc59a94fd02785b
Mateusz Trzuskowski <mateusz.trzuskowski.91@gmail.com>
2021-05-04 19:31:21 +0200

    Ex 2 #1st correct after CR tests

--- views/homework3.py
+++ views/homework3.py
@@ -11,7 +11,10 @@ homework3 = APIRouter()
 templates = Jinja2Templates(directory='templates')
 
 homework3.secret_key = "secret should be long because it is more secjurne"
+login = '4dm1n'
+password = 'NotSoSecurePa$$'
 homework3.saved_token = None
+homework3.saved_session = None
 
 
 # http://127.0.0.1:8000/hello
@@ -26,17 +29,12 @@ def get_hello_html(request: Request):
 @homework3.post("/login_session", status_code=201)
 def post_login_session(response: Response, credentials: HTTPBasicCredentials):
     print(credentials)
-    if credentials.username == '4dm1n' and credentials.password == 'NotSoSecurePa$$':
+    if credentials.username == login and credentials.password == password:
         token_hex = get_token_hex(credentials.username, credentials.password, homework3.secret_key)
-        response.set_cookie(
-            key='session_token',
-            value=token_hex
-        )
-        homework3.saved_token = token_hex
+        response.set_cookie(key='session_token', value=token_hex)
+        homework3.saved_session = token_hex
     else:
         response.status_code = 401
-    return dict(response=response, credentials=credentials)
-
 
 # @homework3.post("/login_session")
 # def post_login_session(auth_header: Optional[str] = Header(None)):
@@ -46,11 +44,11 @@ def post_login_session(response: Response, credentials: HTTPBasicCredentials):
 
 
 @homework3.post("/login_token", status_code=201)
-def post_login_token(*, response: Response, session_token: str = Cookie(None)):
-    print("session_token vs homework3.saved_token")
-    print(session_token)
-    print(homework3.saved_token)
-    if session_token == homework3.saved_token:
-        return dict(token=session_token)
+def post_login_token(response: Response, credentials: HTTPBasicCredentials):
+    print(credentials)
+    if credentials.username == login and credentials.password == password:
+        token_hex = get_token_hex(credentials.username, credentials.password, homework3.secret_key)
+        homework3.saved_token = token_hex
+        return {"token": token_hex}
     else:
         response.status_code = 401
